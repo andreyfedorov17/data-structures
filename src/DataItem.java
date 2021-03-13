e7a456ec43be760b7b5d4d0fac9dbd23c36d9233
Andrey Fedorov <andreyfedorovqq@gmail.com>
2021-03-13 20:38:43 +0300

    add hash table and data item

---
+++ src/DataItem.java
@@ -0,0 +1,11 @@
+public class DataItem {
+    private int iData;
+
+    public DataItem(int iData) {
+        this.iData = iData;
+    }
+
+    public int getKey() {
+        return iData;
+    }
+}
